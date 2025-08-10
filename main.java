75250f6efe80dfaa8df26e21efa11f3546c90fa7
Madhabi das <madhabisayonti1971@gmail.com>
2025-08-10 15:30:31 +0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+  public static void main(String[] args) {
+    String studentName = "Madhabi";
+    int studentID = 25010150;
+    int studentAge = 24;
+    float studentFee = 75.25f;
+    char studentGrade = 'A';
+    System.out.println("Student name: " + studentName);
+    System.out.println("Student id: " + studentID);
+    System.out.println("Student age: " + studentAge);
+    System.out.println("Student fee: " + studentFee);
+    System.out.println("Student grade: " + studentGrade);
+  }
+}
